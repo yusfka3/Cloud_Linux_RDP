079c3586c3c5e6c20e42587ae0da46ceba565d08
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2022-07-06 10:52:29 +0530

    Update nomachine-windows10.sh

--- Docker-Ubuntu/nomachine-windows10.sh
+++ Docker-Ubuntu/nomachine-windows10.sh
@@ -33,25 +33,24 @@ echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!        Requirement
 echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
 echo ""
 echo""
-echo -e "\e[31m[\e[32m*\e[31m]\e[33m Go to : \e[97m https://dashboard.ngrok.com/ \e[m "
+echo -e "\e[31m[\e[32m*\e[31m]\e[93m Go to : \e[97m https://dashboard.ngrok.com/ \e[m "
 echo ""
-echo -e "\e[31m[\e[32m*\e[31m]\e[33m Sign up & get ngrok authtoken \e[m "
+echo -e "\e[31m[\e[32m*\e[31m]\e[93m Sign up & get ngrok authtoken \e[m "
 echo ""
-read -p $'\e[31m[\e[32m*\e[31m]\e[33m Paste Ngrok Authtoken: \e[96m [Ex. 2cEG2LcBt**********WK5Ntc ] : \e[0m' CRP
+read -p $'\e[31m[\e[32m*\e[31m]\e[93m Paste Ngrok Authtoken: \e[96m [Ex. 2cEG2LcBt**********WK5Ntc ] : \e[0m' CRP
 echo ""
 ./ngrok authtoken $CRP 
 
 clear
 echo -e "\e[92m			  							  							  
-          \e[m"
 echo -e " \e[36mWebsite : \e[0m\e[92mwww.onlinehacking.in  \e[93m/  \e[92mwww.onlinehacking.xyz  \e[0m\n"
-     echo ""
+echo ""
 echo -e " \e[97mTelegram : \e[0m\e[93mhttps://t.me/OnlineHacKing \e[0m\n"
 echo ""
 echo "Repo: https://github.com/OnlineHacKing/Cloud_Linux_RDP"
 echo ""
 echo "======================="
-echo "Choose Ngrok Region (for better connection)."
+echo "\e[96mChoose Ngrok Region \e[92m(for better connection).\e[0m\n"
 echo "======================="
 echo "us - United States (Ohio)"
 echo "eu - Europe (Frankfurt)"
@@ -61,7 +60,7 @@ echo "sa - South America (Sao Paulo)"
 echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
 echo ""
-read -p "Choose Ngrok Region: " CRP
+read -p "\e[93mChoose Ngrok Region:\e[0m\n " CRP
 ./ngrok tcp --region $CRP 4000 &>/dev/null &
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
@@ -96,12 +95,12 @@ echo -e "\e[92m
   echo""
 sleep 1
 echo "Use NoMachine VNC App : https://www.nomachine.com/download"
-
+echo -----------------------------------------------------------------
 echo Done! NoMachine Information:
-echo IP Address:
+echo "\e[93m IP Address:\e[0m\n"
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
-echo User: OnlineHacking
-echo Passwd: suman
-
+echo "\e[96m User:\e[0m\n OnlineHacking"
+echo "\e[92m Password:\e[0m\n suman"
+echo -----------------------------------------------------------------
 echo "VM can't connect? Restart Cloud Shell then Re-run script."
 seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
