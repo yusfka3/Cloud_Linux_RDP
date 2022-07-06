cd15245d3223f39d471497422ee6d026d63e498f
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2022-07-06 11:01:23 +0530

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -22,7 +22,7 @@ echo -e "\e[92m
   echo""
    echo""
 echo -e "\e[91m =======================\e[0m"
-echo "Downloading ngrok..."
+echo "    Downloading ngrok..."
 echo -e "\e[91m =======================\e[0m"
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
