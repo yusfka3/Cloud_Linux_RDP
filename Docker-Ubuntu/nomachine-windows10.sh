bee52cd342dd9010ecd427be0e34ecd07318e568
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2022-07-06 10:31:03 +0530

    Update nomachine-windows10.sh

--- Docker-Ubuntu/nomachine-windows10.sh
+++ Docker-Ubuntu/nomachine-windows10.sh
@@ -61,7 +61,8 @@ echo "au - Australia (Sydney)"
 echo "sa - South America (Sao Paulo)"
 echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
-read -p "choose ngrok region: " CRP
+echo ""
+read -p "Choose Ngrok Region: " CRP
 ./ngrok tcp --region $CRP 4000 &>/dev/null &
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
