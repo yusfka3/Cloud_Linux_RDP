b3f108235bacd4cfb7cd3dc5a8469951f4c35984
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2022-07-06 11:22:39 +0530

    Update nomachine-windows10.sh

--- Docker-Ubuntu/nomachine-windows10.sh
+++ Docker-Ubuntu/nomachine-windows10.sh
@@ -41,8 +41,13 @@ read -p $'\e[31m[\e[32m*\e[31m]\e[93m Paste Ngrok Authtoken: \e[96m [Ex. 2cEG2Lc
 echo ""
 ./ngrok authtoken $CRP 
 
-clear
-clear
+clear		  							  							  
+echo -e " \e[36mWebsite : \e[0m\e[92mwww.onlinehacking.in  \e[93m/  \e[92mwww.onlinehacking.xyz  \e[0m\n"
+echo ""
+echo -e " \e[97mTelegram : \e[0m\e[93mhttps://t.me/OnlineHacKing \e[0m\n"
+echo ""
+echo "Repo: https://github.com/OnlineHacKing/Cloud_Linux_RDP"
+echo ""
 echo "======================="
 echo "\e[93mChoose Ngrok Region (for better connection).\e[0m"
 echo "======================="
@@ -70,7 +75,7 @@ echo ""
 echo -e "\e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m Onine Hacking \e[0m"
 echo ""
 sleep 1
-docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=suman -e USER=OnlineHacking --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:windows10
+docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=suman -e USER=OnlineHacking --cap-add=SYS_PTRACE --shm-size=1g BY https:onlinehacking.in
 clear
 echo -e "\e[92m 
          ██████╗ ███╗   ██╗██╗     ██╗███╗   ██╗███████╗    
@@ -91,10 +96,10 @@ sleep 1
 echo "Use NoMachine VNC App : https://www.nomachine.com/download"
 echo -----------------------------------------------------------------
 echo Done! NoMachine Information:
-echo "\e[93m IP Address:\e[0m\n"
+echo -e "\e[93m IP Address:\e[0m\n"
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
-echo "\e[96m User:\e[0m\n OnlineHacking"
-echo "\e[92m Password:\e[0m\n suman"
+echo -e "\e[96m User:\e[0m\n OnlineHacking"
+echo -e "\e[92m Password:\e[0m\n suman"
 echo -----------------------------------------------------------------
 echo "VM can't connect? Restart Cloud Shell then Re-run script."
 seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
