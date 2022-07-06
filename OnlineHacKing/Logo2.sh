c282d1a3062c3c73b8e603ed5acd8ecb9d30119c
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2022-07-06 18:02:57 +0530

    Update Logo2.sh

--- OnlineHacKing/Logo2.sh
+++ OnlineHacKing/Logo2.sh
@@ -11,19 +11,19 @@ echo -e ""
       echo -e $'\e[1;34m================================================================== > > >\e[0m '
 printf " \e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m " 
 echo " "
-printf "\e[1;31m[\e[0m\e[1;92m01\e[0m\e[1;31m] \e[0m\e[1;37;44m Kali Linux \e[0m  \e[95m[\e[32m2 Type\e[95m]\e[0m       \e[1;31m[\e[0m\e[1;92m05\e[0m\e[1;31m] \e[0m\e[1;48;5;21m Debian Linux XFCE4 \e[0m \e[0m \e[95m[\e[93mWeb VNC\e[95m]\e[0m "
+printf "\e[1;31m[\e[0m\e[1;92m01\e[0m\e[1;31m] \e[0m\e[1;37;44m Kali Linux \e[0m  \e[95m[\e[93m2 Type\e[95m]\e[0m         \e[1;31m[\e[0m\e[1;92m05\e[0m\e[1;31m] \e[0m\e[1;48;5;21m Debian Linux XFCE4 \e[0m \e[0m \e[95m[\e[93mWeb VNC\e[95m]\e[0m "
 echo
 printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"     
 echo
-printf "\e[1;31m[\e[0m\e[1;92m02\e[0m\e[1;31m] \e[0m\e[1;37;41m Ubuntu MATE Green \e[0m        \e[1;31m[\e[0m\e[1;92m10\e[0m\e[1;31m] \e[0m\e[1;48;5;200m Ubuntu Desktop lxde \e[0m \e[95m[\e[93mWeb VNC\e[95m]\e[0m     "
+printf "\e[1;31m[\e[0m\e[1;92m02\e[0m\e[1;31m] \e[0m\e[1;37;41m Ubuntu MATE Green \e[0m              \e[1;31m[\e[0m\e[1;92m10\e[0m\e[1;31m] \e[0m\e[1;48;5;200m Ubuntu Desktop lxde \e[0m \e[95m[\e[93mWeb VNC\e[95m]\e[0m     "
 echo
 printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"
 echo
-printf "\e[1;31m[\e[0m\e[1;92m03\e[0m\e[1;31m] \e[0m\e[1;91;107m Debian - Windows 10 Theme \e[0m       \e[1;31m[\e[0m\e[1;92m09\e[0m\e[1;31m] \e[0m\e[1;91;102m XFCE4 Desktop \e[0m    "
+printf "\e[1;31m[\e[0m\e[1;92m03\e[0m\e[1;31m] \e[0m\e[1;91;107m Debian - Windows 10 Theme \e[0m   \e[1;31m[\e[0m\e[1;92m09\e[0m\e[1;31m] \e[0m\e[1;91;102m XFCE4 Desktop \e[0m    "
 echo
 printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"
 echo
-printf "\e[1;31m[\e[0m\e[1;92m04\e[0m\e[1;31m] \e[0m\e[1;34;103m Ubuntu 20.04 XFCE4 WineHQ \e[0m        \e[1;31m[\e[0m\e[1;92m99\e[0m\e[1;31m] \e[0m\e[1;33mExit\e[0m   "
+printf "\e[1;31m[\e[0m\e[1;92m04\e[0m\e[1;31m] \e[0m\e[1;34;103m Ubuntu 20.04 XFCE4 WineHQ \e[0m    \e[1;31m[\e[0m\e[1;92m99\e[0m\e[1;31m] \e[0m\e[1;33mExit\e[0m   "
 echo
 printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"   
 echo " "
