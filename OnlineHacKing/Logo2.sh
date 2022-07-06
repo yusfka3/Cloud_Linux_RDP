ef0cd4f61e867f5d5aa46a484372ecfcfaaad2b6
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2022-07-06 18:06:27 +0530

    Update Logo2.sh

--- OnlineHacKing/Logo2.sh
+++ OnlineHacKing/Logo2.sh
@@ -15,11 +15,11 @@ printf "\e[1;31m[\e[0m\e[1;92m01\e[0m\e[1;31m] \e[0m\e[1;37;44m Kali Linux \e[0m
 echo
 printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"     
 echo
-printf "\e[1;31m[\e[0m\e[1;92m02\e[0m\e[1;31m] \e[0m\e[1;37;41m Ubuntu MATE Green \e[0m              \e[1;31m[\e[0m\e[1;92m10\e[0m\e[1;31m] \e[0m\e[1;48;5;200m Ubuntu Desktop lxde \e[0m \e[95m[\e[93mWeb VNC\e[95m]\e[0m     "
+printf "\e[1;31m[\e[0m\e[1;92m02\e[0m\e[1;31m] \e[0m\e[1;37;41m Ubuntu MATE Green \e[0m            \e[1;31m[\e[0m\e[1;92m10\e[0m\e[1;31m] \e[0m\e[1;48;5;200m Ubuntu Desktop lxde \e[0m \e[95m[\e[93mWeb VNC\e[95m]\e[0m     "
 echo
 printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"
 echo
-printf "\e[1;31m[\e[0m\e[1;92m03\e[0m\e[1;31m] \e[0m\e[1;91;107m Debian - Windows 10 Theme \e[0m   \e[1;31m[\e[0m\e[1;92m09\e[0m\e[1;31m] \e[0m\e[1;91;102m XFCE4 Desktop \e[0m    "
+printf "\e[1;31m[\e[0m\e[1;92m03\e[0m\e[1;31m] \e[0m\e[1;91;107m Debian - Windows 10 Theme \e[0m    \e[1;31m[\e[0m\e[1;92m09\e[0m\e[1;31m] \e[0m\e[1;91;102m XFCE4 Desktop \e[0m    "
 echo
 printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"
 echo
