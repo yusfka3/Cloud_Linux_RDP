1ed27fa188302883ea1a5f706d767937c4fa98f1
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2022-07-07 12:17:42 +0530

    Update KaliLinux.sh

--- KaliLinux.sh
+++ KaliLinux.sh
@@ -4,10 +4,31 @@ echo
 clear
 echo ""
 clear
-bash OnlineHacKing/1.sh
-clear
-bash OnlineHacKing/Logo.sh
-bash OnlineHacKing/Logo2.sh
+#clolors
+white='\e[1;37m'
+green='\e[0;32m'
+blue='\e[1;34m'
+red='\e[1;31m'
+yellow='\e[1;33m' 
+echo ""
+banner() {
+echo -e ""
+      echo -e $'\e[1;34m================================================================== > > >\e[0m '
+printf " \e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m " 
+echo " "
+printf "\e[1;31m[\e[0m\e[1;92m01\e[0m\e[1;31m] \e[0m\e[1;48;5;21m Kali Linux (Use NoMachine VNC Application) \e[0m \e[0m \e[95m[\e[93mSmooth\e[95m]\e[0m "
+echo
+printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"     
+echo
+printf "\e[1;31m[\e[0m\e[1;92m02\e[0m\e[1;31m] \e[0m\e[1;37;41m Kali Linux (Use Remote Desktop Application ) \e[0m \e[0m \e[95m[\e[93mUpdating\e[95m]\e[0m  "
+echo
+printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m" 
+echo " "
+      echo -e $'\e[1;34m================================================================== > > >\e[0m '
+      echo -e $' ' 
+      echo ""                                                
+      }
+       banner
 
                  echo -e $''   
                  read -p $'\e[1;91m[\e[0m\e[1;91m *** \e[0m\e[1;96m]\e[0m\e[1;92m  You Want to Linux Desktop Option  \e[1;91m  > : \e[0m' option
