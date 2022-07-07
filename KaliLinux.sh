abb8cf6606b863c6074120998c8c88dbbf71d304
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2022-07-07 19:11:51 +0530

    Update KaliLinux.sh

--- KaliLinux.sh
+++ KaliLinux.sh
@@ -11,6 +11,34 @@ kill -9 $php
 kill -9 $ngrok
 clear
 bash OnlineHacKing/Logo3.sh
+echo ""
+echo "" 
+echo -e "\e[33m
+      ██╗  ██╗ █████╗ ██╗     ██╗    ██╗     ██╗███╗   ██╗██╗   ██╗██╗  ██╗
+      ██║ ██╔╝██╔══██╗██║     ██║    ██║     ██║████╗  ██║██║   ██║╚██╗██╔╝
+      █████╔╝ ███████║██║     ██║    ██║     ██║██╔██╗ ██║██║   ██║ ╚███╔╝ 
+      ██╔═██╗ ██╔══██║██║     ██║    ██║     ██║██║╚██╗██║██║   ██║ ██╔██╗ 
+      ██║  ██╗██║  ██║███████╗██║    ███████╗██║██║ ╚████║╚██████╔╝██╔╝ ██╗
+      ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝    ╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═╝ \e[0m "
+ 
+  echo""  
+  echo -e $'\e[1;33m\e[0m\e[1;31m    ██████████\e[0m'"\e[1;37m██████████"'\e[1;33m\e[0m\e[0;32m██████████\e[0m' '\e[95m KALI LINUX CLOUD RDP DESKTOP \e[0m''\e[1;37m\e[0m\e[1;37m \e[0m'                                       
+  echo ""
+echo -e ""
+      echo -e $'\e[1;34m================================================================== > > >\e[0m '
+printf " \e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m " 
+echo " "
+printf "\e[1;31m[\e[0m\e[1;92m01\e[0m\e[1;31m] \e[0m\e[1;48;5;21m Kali Linux (Use NoMachine VNC Application) \e[0m \e[0m \e[95m[\e[93mSmooth\e[95m]\e[0m "
+echo
+printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"     
+echo
+printf "\e[1;31m[\e[0m\e[1;92m02\e[0m\e[1;31m] \e[0m\e[1;37;41m Kali Linux (Use Remote Desktop Application ) \e[0m \e[0m \e[95m[\e[93mUpdating\e[95m]\e[0m  "
+echo
+printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m" 
+echo " "
+      echo -e $'\e[1;34m================================================================== > > >\e[0m '
+      echo -e $' ' 
+      echo "" 
 
                  echo -e $''   
                  read -p $'\e[1;91m[\e[0m\e[1;91m *** \e[0m\e[1;96m]\e[0m\e[1;92m   You Want to Linux Desktop Option  \e[1;91m  > : \e[0m' option
