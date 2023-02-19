5860148a6910f9071c0a93b24a8aa56187fa6e44
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2023-02-19 19:48:42 +0530

    Update by Suman

--- Docker-Ubuntu/ngrok.sh
+++ Docker-Ubuntu/ngrok.sh
@@ -2,7 +2,6 @@
 stty intr ""
 stty quit ""
 stty susp undef
-
 clear
 rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
 echo "======================="
@@ -10,4 +9,3 @@ echo "Downloading ngrok..."
 echo "======================="
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
-
