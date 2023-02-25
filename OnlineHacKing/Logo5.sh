44c724f5638e8fed016acbabd188a932a6a1be08
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2023-02-25 20:54:43 +0530

    Create Logo5.sh

---
+++ OnlineHacKing/Logo5.sh
@@ -0,0 +1,14 @@
+echo ""
+			   printf ' \e[1;31m[\e[0m\e[1;92m01\e[0m\e[1;31m]  \e[91m Click thsi Linux :- \e[97m https://remotedesktop.google.com/headless \e[0m '
+			   echo ""
+			   printf " \e[1;31m[\e[0m\e[1;92m02\e[0m\e[1;31m]  \e[92m Click Begin , Click Before , Click Authorize \e[0m " 
+			   echo " "
+			   printf " \e[1;31m[\e[0m\e[1;92m03\e[0m\e[1;31m]  \e[92m Copy \e[96m Debian Linux \e[0m \e[92m and Paste on terminal/shell google console \e[0m " 
+			   echo " "
+			   printf ' \e[1;31m[\e[0m\e[1;92m04\e[0m\e[1;31m]  \e[31m[\e[32m*\e[31m]\e[33m Enter The Debian Linux Code : \e[0m'
+			   echo ""
+			   printf " \e[1;31m[\e[0m\e[1;92m05\e[0m\e[1;31m]  \e[91m Click this Linux :- \e[97m https://remotedesktop.google.com/access \e[0m " 
+			   echo ""
+			   echo -e
+			   echo -e "\e[96m=====================================================================\e[92m"
+         echo ""
