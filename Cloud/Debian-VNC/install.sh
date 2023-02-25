2e28683add6d2b0f6c19fd6485f2391b420729e4
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2023-02-25 11:03:05 +0530

    Update install.sh

--- Cloud/Debian-VNC/install.sh
+++ Cloud/Debian-VNC/install.sh
@@ -1,80 +1,16 @@
 #!/bin/sh
-#Youtube: https://youtube.com/channel/onlinehacking2
-#Facebook: https://fb.me/onlinehacking
-echo -e "\e[92m 
-         ██████╗ ███╗   ██╗██╗     ██╗███╗   ██╗███████╗    
-        ██╔═══██╗████╗  ██║██║     ██║████╗  ██║██╔════╝    
-        ██║   ██║██╔██╗ ██║██║     ██║██╔██╗ ██║█████╗      
-        ██║   ██║██║╚██╗██║██║     ██║██║╚██╗██║██╔══╝      
-        ╚██████╔╝██║ ╚████║███████╗██║██║ ╚████║███████╗    
-         ╚═════╝ ╚═╝  ╚═══╝╚══════╝╚═╝╚═╝  ╚═══╝╚══════╝    
-                                                            
-     ██╗  ██╗ █████╗  ██████╗██╗  ██╗██╗███╗   ██╗ ██████╗   
-     ██║  ██║██╔══██╗██╔════╝██║ ██╔╝██║████╗  ██║██╔════╝   
-     ███████║███████║██║     █████╔╝ ██║██╔██╗ ██║██║  ███╗  
-     ██╔══██║██╔══██║██║     ██╔═██╗ ██║██║╚██╗██║██║   ██║  
-     ██║  ██║██║  ██║╚██████╗██║  ██╗██║██║ ╚████║╚██████╔╝  
-     ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝  \e[95;1m \e[0m\n"
-  echo""
-   echo""
-echo -e "\e[91m =======================\e[0m"
-echo "  Downloading ngrok..."
-echo -e "\e[91m =======================\e[0m"
-echo -e ""
-wget -O ngrok-stable-linux-amd64.zip "https://github.com/OnlineHacKing/Cloud_Linux_RDP/raw/main/Cloud/Debian-VNC/ngrok-stable-linux-amd64.zip?raw=true" > /dev/null 2>&1
+#KhanhNguyen9872
+#Youtube: https://youtube.com/channel/UCG48mG78znU95DSxyCBffOg
+#Facebook: https://fb.me/khanh10a1
+#Phone: +84937927513
+echo "Preparing...."
+wget -O ngrok-stable-linux-amd64.zip "https://github.com/KhanhNguyen9872/Debian_VPS_Google_Shell/blob/main/ngrok-stable-linux-amd64.zip?raw=true" > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1; rm ngrok-stable-linux-amd64.zip 2> /dev/null
 sudo mv ./ngrok /bin/ngrok; chmod 777 /bin/ngrok
-
-lear
-echo ""
-echo -e "\e[92m 
- ███▄    █   ▄████  ██▀███   ▒█████   ██ ▄█▀
- ██ ▀█   █  ██▒ ▀█▒▓██ ▒ ██▒▒██▒  ██▒ ██▄█▒ 
-▓██  ▀█ ██▒▒██░▄▄▄░▓██ ░▄█ ▒▒██░  ██▒▓███▄░ 
-▓██▒  ▐▌██▒░▓█  ██▓▒██▀▀█▄  ▒██   ██░▓██ █▄ 
-▒██░   ▓██░░▒▓███▀▒░██▓ ▒██▒░ ████▓▒░▒██▒ █▄
-░ ▒░   ▒ ▒  ░▒   ▒ ░ ▒▓ ░▒▓░░ ▒░▒░▒░ ▒ ▒▒ ▓▒
-░ ░░   ░ ▒░  ░   ░   ░▒ ░ ▒░  ░ ▒ ▒░ ░ ░▒ ▒░
-   ░   ░ ░ ░ ░   ░   ░░   ░ ░ ░ ░ ▒  ░ ░░ ░ 
-         ░       ░    ░         ░ ░  ░  ░   
-\e[0m\n"
-echo ""
-echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
-echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!        Requirement Ngrok Token       !!\e[0m'
-echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
-echo ""
-echo ""
-echo -e "\e[31m[\e[32m*\e[31m]\e[93m Go to : \e[97m https://dashboard.ngrok.com/get-started/your-authtoken \e[m "
-echo ""
-echo -e "\e[31m[\e[32m*\e[31m]\e[93m Sign up & get ngrok authtoken \e[m "
-echo ""
-read -p $'\e[31m[\e[32m*\e[31m]\e[93m Paste Ngrok Authtoken: \e[96m [Ex. 2cEG2LcBt**********WK5Ntc ] : \e[0m' key
-echo ""
+read -p "INSERT authtoken ngrok: " key
 ngrok authtoken $key
 echo ""
-clear
-echo ""
-echo ""
-echo -e "\e[92m 
-         ██████╗ ███╗   ██╗██╗     ██╗███╗   ██╗███████╗    
-        ██╔═══██╗████╗  ██║██║     ██║████╗  ██║██╔════╝    
-        ██║   ██║██╔██╗ ██║██║     ██║██╔██╗ ██║█████╗      
-        ██║   ██║██║╚██╗██║██║     ██║██║╚██╗██║██╔══╝      
-        ╚██████╔╝██║ ╚████║███████╗██║██║ ╚████║███████╗    
-         ╚═════╝ ╚═╝  ╚═══╝╚══════╝╚═╝╚═╝  ╚═══╝╚══════╝    
-                                                            
-     ██╗  ██╗ █████╗  ██████╗██╗  ██╗██╗███╗   ██╗ ██████╗   
-     ██║  ██║██╔══██╗██╔════╝██║ ██╔╝██║████╗  ██║██╔════╝   
-     ███████║███████║██║     █████╔╝ ██║██╔██╗ ██║██║  ███╗  
-     ██╔══██║██╔══██║██║     ██╔═██╗ ██║██║╚██╗██║██║   ██║  
-     ██║  ██║██║  ██║╚██████╗██║  ██╗██║██║ ╚████║╚██████╔╝  
-     ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝  \e[95;1m \e[0m\n"
-  echo""
-   echo""
-echo -e "\e[91m =======================\e[0m"
-echo "  Installing Linux (Debian amd64)...."
-echo -e "\e[91m =======================\e[0m"
-echo -e ""
+echo "Installing Linux (Debian amd64)...."
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update -y > /dev/null 2>&1
@@ -92,9 +28,9 @@ fi
 echo "set fish_greeting" > ~/.config/fish/config.fish
 chmod -R 777 ~/.config 2> /dev/null
 sudo printf '#!/bin/bash\ndbus-launch &> /dev/null\nautocutsel -fork\nxfce4-session\n' > ~/.vnc/xstartup
-wget -O startvps.sh "https://github.com/OnlineHacKing/Cloud_Linux_RDP/raw/main/Cloud/Debian-VNC/startvps.sh" 2> /dev/null
-wget -O setupPS.sh "https://github.com/OnlineHacKing/Cloud_Linux_RDP/raw/main/Cloud/Debian-VNC/setupPS.sh" 2> /dev/null
-wget -O apache2.conf "https://github.com/OnlineHacKing/Cloud_Linux_RDP/raw/main/Cloud/Debian-VNC/apache2.conf" 2> /dev/null
+wget -O startvps.sh "https://raw.githubusercontent.com/KhanhNguyen9872/Debian_VPS_Google_Shell/main/startvps.sh" 2> /dev/null
+wget -O setupPS.sh "https://raw.githubusercontent.com/KhanhNguyen9872/Debian_VPS_Google_Shell/main/setupPS.sh" 2> /dev/null
+wget -O apache2.conf "https://raw.githubusercontent.com/KhanhNguyen9872/Debian_VPS_Google_Shell/main/apache2.conf" 2> /dev/null
 wget -O vscode.deb "https://github.com/KhanhNguyen9872/Debian_VPS_Google_Shell/blob/main/app/vscode_1.66.1_amd64.deb?raw=true" 2> /dev/null
 sudo mv ./startvps.sh /bin/startvps 2> /dev/null
 sudo rm -rf ~/.bashrc 2> /dev/null
@@ -126,39 +62,11 @@ sudo apt update -y > /dev/null 2>&1
 sudo apt autoremove -y
 if [ ! -d /usr/share/themes/Windows-10-Dark-master ] 2> /dev/null; then
   cd /usr/share/themes/ 2> /dev/null
-  wget -O Windows-10-Dark-master.zip "https://github.com/OnlineHacKing/Cloud_Linux_RDP/raw/main/Cloud/Debian-VNC/app/Windows-10-Dark-master.zip?raw=true" 2> /dev/null
+  wget -O Windows-10-Dark-master.zip "https://github.com/KhanhNguyen9872/Debian_VPS_Google_Shell/blob/main/app/Windows-10-Dark-master.zip?raw=true" 2> /dev/null
   unzip -qq Windows-10-Dark-master.zip 2> /dev/null
   rm -f Windows-10-Dark-master.zip 2> /dev/null
 fi
 cd $HOME 2> /dev/null
 clear
 printf "\n\n\n - Installation completed!\n Run: [startvps] to start VNC Server!\n\n"
-
-
-echo -e "\e[92m 
-         ██████╗ ███╗   ██╗██╗     ██╗███╗   ██╗███████╗    
-        ██╔═══██╗████╗  ██║██║     ██║████╗  ██║██╔════╝    
-        ██║   ██║██╔██╗ ██║██║     ██║██╔██╗ ██║█████╗      
-        ██║   ██║██║╚██╗██║██║     ██║██║╚██╗██║██╔══╝      
-        ╚██████╔╝██║ ╚████║███████╗██║██║ ╚████║███████╗    
-         ╚═════╝ ╚═╝  ╚═══╝╚══════╝╚═╝╚═╝  ╚═══╝╚══════╝    
-                                                            
-     ██╗  ██╗ █████╗  ██████╗██╗  ██╗██╗███╗   ██╗ ██████╗   
-     ██║  ██║██╔══██╗██╔════╝██║ ██╔╝██║████╗  ██║██╔════╝   
-     ███████║███████║██║     █████╔╝ ██║██╔██╗ ██║██║  ███╗  
-     ██╔══██║██╔══██║██║     ██╔═██╗ ██║██║╚██╗██║██║   ██║  
-     ██║  ██║██║  ██║╚██████╗██║  ██╗██║██║ ╚████║╚██████╔╝  
-     ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝  \e[95;1m \e[0m\n"
-  echo""
-sleep 1
-echo -e "\e[96mWebsite : \e[93mhttps://www.termux.xyz/ \e[0m"
-echo ""
-echo -----------------------------------------------------------------
-echo ""
-printf "\e[31m[\e[32m★\e[31m]\e[92m Open Remote Desktop App (Android/Windows). Enter RDP Address and Connect RDP \e[97m"
-echo ""
-echo -e "\e[96m Installation completed! \e[97m Run: [startvps] to start VNC Server!"
-echo -e ""
-echo -----------------------------------------------------------------
-
 exit 0
